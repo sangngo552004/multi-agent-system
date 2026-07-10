3836db648cbef2c6657792437cd08664b63ca261
Ngo Tan Sang <sangngo552004@gmail.com>
2026-07-10 14:05:48 +0700

    ci: disable flyway in tests to fix contextLoads

--- backend-core/src/test/java/com/tttn/backend_core/BackendCoreApplicationTests.java
+++ backend-core/src/test/java/com/tttn/backend_core/BackendCoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.tttn.backend_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendCoreApplicationTests {
 
   @Test
