9dc136667774ad7424d0c7a5dc038c7f0ececd13
Ngo Tan Sang <sangngo552004@gmail.com>
2026-07-11 11:00:41 +0700

    fix: unit test autoconfigureTestDb

--- backend-core/src/test/java/com/tttn/backend_core/BackendCoreApplicationTests.java
+++ backend-core/src/test/java/com/tttn/backend_core/BackendCoreApplicationTests.java
@@ -1,13 +1,17 @@
 package com.tttn.backend_core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase
+@TestPropertySource(
+    properties = {
+      "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+      "spring.datasource.driverClassName=org.h2.Driver"
+    })
 class BackendCoreApplicationTests {
 
   @Test
